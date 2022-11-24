1105d7ba33e39847395f538776ba6d59a39904f3
savitadevi <83698279+savitadevi@users.noreply.github.com>
2022-11-24 15:43:15 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,24 +1,19 @@
-package userinput;
+package srings;
 import java.util.Scanner;
 
 public class string {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the length of array");
-		int length=sc.nextInt();
-		String names[]=new String[length];
-        System.out.println("enter the value");
-        for(int i=0; i<names.length;i++){
-        	names[i]=sc.next();
-        }
-        
-        for(int i=0;i<names.length;i++){
-       
-        	System.out.println(names[i]);
-        }
-        
+		
+		String name="full stack web developer";
+		System.out.println(name.toUpperCase());
+		System.out.println(name.replace('w', 'v'));
+		System.out.println(name.indexOf('u'));
+		System.out.println(name.indexOf('b'));
+		System.out.println(name.replaceAll(" ","" ));
+		System.out.println("\"full stack web developer\"");
+
 	}
 
 }
