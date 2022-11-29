c0f85cbbd59a4320b92ed7e3109b2a395a3277fd
savitadevi <83698279+savitadevi@users.noreply.github.com>
2022-11-29 11:25:07 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,30 @@
+package whileloop;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		//while
+//        int i=1;
+//        while(i<=10) {
+//        	System.out.println(i);
+//        	i+=1;
+//        }
+		
+		
+		
+		//dowhile
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=10);
+	}
+
+}
